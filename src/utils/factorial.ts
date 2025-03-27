31333af412f7e117f91dfe0d2753692ae0258140
Lucas <afs0028447@agderskolen.no>
2025-03-27 12:18:28 +0100

    error

--- src/utils/factorial.ts
+++ src/utils/factorial.ts
@@ -1,5 +1,5 @@
 export function factorial(n: number): number {
   if (n < 0) throw new Error("Factorial is not defined for negative numbers");
-  if (n === 0 || n === 1) return 1;
+  if (n === 0 || n === 1) return 0;
   return n * factorial(n - 1);
 }
